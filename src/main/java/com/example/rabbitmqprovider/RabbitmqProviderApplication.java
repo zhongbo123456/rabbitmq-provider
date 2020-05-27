2b62a2d245a22ce7c7513013877db86e828d3cb9
bo.zhong <bo.zhong@langyangtech.com>
2020-05-27 15:08:28 +0800

    Initial commit

---
+++ src/main/java/com/example/rabbitmqprovider/RabbitmqProviderApplication.java
@@ -0,0 +1,16 @@
+package com.example.rabbitmqprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bo.zhong
+ */
+@SpringBootApplication
+public class RabbitmqProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqProviderApplication.class, args);
+    }
+
+}
